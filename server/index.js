7f6fe2eafd3e55df8592e49dccb8e668040dad1b
Sree-Varshitha2 <sreevarshitha04@gmail.com>
2026-03-28 10:44:07 +0530

    Add files via upload

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const WebSocket = require("ws");
+
+const wss = new WebSocket.Server({ port: 8080 });
+
+wss.on("connection", (ws) => {
+  console.log("Client connected");
+
+  ws.on("message", (message) => {
+    // broadcast to all clients
+    wss.clients.forEach((client) => {
+      if (client.readyState === WebSocket.OPEN) {
+        client.send(message.toString());
+      }
+    });
+  });
+});
+
+console.log("Server running on ws://localhost:8080");
